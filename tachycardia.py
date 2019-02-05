68091aa762ab1b8271510ba5c7b55fad0d939a7e
Kim <kimberly.lennox@duke.edu>
2019-02-05 15:19:23 -0500

    Test accounts for leading and trailing spaces

--- tachycardia.py
+++ tachycardia.py
@@ -1,5 +1,7 @@
 def is_tachycardic(s):
     s=s.lower() #Convert string to completely lowercase
+    s=s.lstrip() #Remove all leading spaces
+    s=s.rstrip() #Remove all trailing spaces
     if s == "tachycardic":
         R = True
     else:
