b5e7f415d7354da9ab96c5be81a029be4fb13d15
Kim <kimberly.lennox@duke.edu>
2019-02-05 15:29:55 -0500

    Added comments and function message, conformed to PEP8 style guide

--- tachycardia.py
+++ tachycardia.py
@@ -1,7 +1,11 @@
 def is_tachycardic(s):
-    s=s.lower() #Convert string to completely lowercase
-    s=s.lstrip() #Remove all leading spaces
-    s=s.rstrip() #Remove all trailing spaces
+
+    """This function takes a test input string and determines if
+    the string is the word tachycardic"""
+
+    s = s.lower()  # Convert string to completely lowercase
+    s = s.lstrip()  # Remove all leading spaces
+    s = s.rstrip()  # Remove all trailing spaces
     if s == "tachycardic":
         R = True
     else:
